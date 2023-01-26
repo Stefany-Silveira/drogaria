e02d58d010381ca840f466ae38d01bc9177a203c
stefany <stefany.silveira21@gmail.com>
2023-01-26 10:18:34 -0300

    update DrogariaApplication

--- src/main/java/br/com/drogaria/DrogariaApplication.java
+++ src/main/java/br/com/drogaria/DrogariaApplication.java
@@ -1,4 +1,4 @@
-package br.com.itau.drogaria.drogaria;
+package br.com.drogaria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
