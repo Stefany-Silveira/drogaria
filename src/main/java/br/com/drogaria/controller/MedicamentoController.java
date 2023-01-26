56d084e66b04eb13c2e13812d835c7ae0afc2ad0
stefany <stefany.silveira21@gmail.com>
2023-01-26 10:17:05 -0300

    update MedicamentoController

--- src/main/java/br/com/drogaria/controller/MedicamentoController.java
+++ src/main/java/br/com/drogaria/controller/MedicamentoController.java
@@ -1,10 +1,9 @@
-package br.com.itau.drogaria.drogaria.controller;
+package br.com.drogaria.controller;
 
-import br.com.itau.drogaria.drogaria.controller.request.MedicamentoRequest;
-import br.com.itau.drogaria.drogaria.controller.response.MedicamentoResponse;
-import br.com.itau.drogaria.drogaria.repository.MedicamentoRepository;
-import br.com.itau.drogaria.drogaria.model.Medicamento;
-import br.com.itau.drogaria.drogaria.service.MedicamentoService;
+import br.com.drogaria.controller.request.MedicamentoRequest;
+import br.com.drogaria.controller.response.MedicamentoResponse;
+import br.com.drogaria.model.Medicamento;
+import br.com.drogaria.service.MedicamentoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -15,42 +14,41 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("medicamentos")
+@RequestMapping("/medicamentos")
 @RequiredArgsConstructor
 @Api(value="API REST Medicamentos")
 @CrossOrigin(origins="*")
 public class MedicamentoController {
 
     private final MedicamentoService medicamentoService;
-    private final MedicamentoRepository medicamentoRepository;
 
     @GetMapping
-    @ApiOperation(value="Retornar uma lista de medicamentos")
+    @ApiOperation(value = "Retornar uma lista de medicamentos")
     public ResponseEntity<List<MedicamentoResponse>> listAll() {
         return ResponseEntity.ok(medicamentoService.listAll());
     }
 
-    @GetMapping(path="/{id}")
-    @ApiOperation(value="Retornar medicamento por id")
-    public ResponseEntity<Medicamento> findById(@PathVariable Long id){
+    @GetMapping(path = "/{id}")
+    @ApiOperation(value = "Retornar um medicamento por id")
+    public ResponseEntity<Medicamento> findById(@PathVariable Long id) {
         Medicamento medicamento = medicamentoService.findById(id);
         return ResponseEntity.ok().body(medicamento);
     }
 
     @PostMapping
-    @ApiOperation(value="Inserir um novo medicamento")
+    @ApiOperation(value = "Inserir um medicamento")
     public ResponseEntity<MedicamentoResponse> save(@RequestBody MedicamentoRequest medicamentoRequest) {
         return new ResponseEntity<>(medicamentoService.save(medicamentoRequest), HttpStatus.CREATED);
     }
 
-    @PutMapping(path="/{id}")
-    @ApiOperation(value="Atualizar um medicamento")
-    public ResponseEntity<MedicamentoResponse> update(@RequestBody MedicamentoRequest medicamento) {
-        return ResponseEntity.status(HttpStatus.OK).body(medicamentoService.save(medicamento));
+    @PutMapping
+    @ApiOperation(value = "Atualizar um medicamento")
+    public ResponseEntity<Medicamento> update(@RequestBody Medicamento medicamento) {
+        return ResponseEntity.status(HttpStatus.OK).body(medicamentoService.update(medicamento));
     }
 
     @DeleteMapping(path = "/{id}")
-    @ApiOperation(value="Deletar um medicamento")
+    @ApiOperation(value = "Deletar um medicamento")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         medicamentoService.delete(id);
         return ResponseEntity.ok().build();
