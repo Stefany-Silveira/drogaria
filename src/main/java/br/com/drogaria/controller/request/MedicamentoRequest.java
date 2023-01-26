a99c505b7bf32a0837e395f489eb30f1e02398ff
stefany <stefany.silveira21@gmail.com>
2023-01-26 10:17:44 -0300

    update MedicamentoRequest

--- src/main/java/br/com/drogaria/controller/request/MedicamentoRequest.java
+++ src/main/java/br/com/drogaria/controller/request/MedicamentoRequest.java
@@ -1,4 +1,4 @@
-package br.com.itau.drogaria.drogaria.controller.request;
+package br.com.drogaria.controller.request;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -11,15 +11,10 @@ public class MedicamentoRequest {
 
     @ApiModelProperty(value = "Nome do medicamento")
     private String nome;
-
-    @ApiModelProperty(value = "Fabricante do medicamento")
-
+    @ApiModelProperty(value = "Nome do fabricante")
     private String fabricante;
-
     @ApiModelProperty(value = "Quantidade do medicamento")
     private int quantidade;
-
     @ApiModelProperty(value = "Preço do medicamento")
     private BigDecimal preco;
-
 }
